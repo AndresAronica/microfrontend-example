cca68f0265eefc4fab4f434def82679da3e85fc1
Andres Aronica <andresaronica@gmail.com>
2023-05-24 17:03:26 -0300

    added header, separate css classnames for prod

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+	productionPrefix: 'co',
+});
 
 export default () => {
-	return <div>
-		<h1>Hi there!! :D</h1>
-		<hr />
-		<MarketingApp />
-	</div>
+	return (
+		<BrowserRouter>
+			<StylesProvider generateClassName={generateClassName}>
+				<div>
+					<Header />
+					<MarketingApp />
+				</div>
+			</StylesProvider>
+		</BrowserRouter>
+	);
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+// cuando buildea para prod, en vez de nombrar el css como jss1,2,3
+// los va a nombrar ma1,2,3
+// para evitar la colision de clases con los otros proyectos
+const generateClassName = createGenerateClassName({
+	productionPrefix: 'ma',
+});
+
 export default () => {
 	return <div>
-		<StylesProvider>
-			<BrowserRouter>
+		<BrowserRouter>
+			<StylesProvider generateClassName={generateClassName}>
 				<Switch>
 					<Route exact path="/pricing" component={Pricing} />
 					<Route path="/" component={Landing} />
 				</Switch>
-			</BrowserRouter>
-		</StylesProvider>
+			</StylesProvider>
+		</BrowserRouter>
 	</div>
 }
